f5552d1e38b6f16334209d7b3d0d4ca00377ff95
ALDENCO <33554727+ALDENCO@users.noreply.github.com>
2018-08-22 18:13:54 -0600

    Add files via upload

--- request-gif.js
+++ request-gif.js
@@ -1,4 +1,3 @@
-
 var ajaxson5 = new Vue({
     el: '#mount-point',
     data: function () {
@@ -7,6 +6,8 @@ var ajaxson5 = new Vue({
             errorMessage: null,
             loading: false,
             imgSrc: null,
+            gif: gif.url,
+           
         };
     },
     methods: {
@@ -15,17 +16,22 @@ var ajaxson5 = new Vue({
             var searchQuery = ""; // TODO should be e.g. "dance"
 
             // configure a few parameters to attach to our request
-            var api_key = "dc6zaTOxFJmzC";
+            var api_key = "n2KJgt6XJN0Z9Fj6D8924vDw8vOAp5Tt";
             var tag = ""; // TODO should be e.g. "jackson 5 dance"
+            var gif_id = "gif.slug";
 
             // TODO what do we want this URL to be?
-            fetch(`http://example.com?api_key=${api_key}&tag=${tag}`)
+            fetch(`https://api.giphy.com/v1/gifs/random?api_key=${api_key}&tag=${tag}`)
                 .then(response => response.ok ? response.json() : Promise.reject(response))
                 .then(results => {
                     // if the response comes back successfully, the code in here will execute.
 
                     console.log("we received a response!");
                     console.log(results);
+
+                    this.imgSrc = results(gif_id);
+
+                    return `https://media.giphy.com/media/${gif_id}/giphy.gif`;
                     // TODO
                     // 1. set the imgSrc value in our data to the GIF's image_url inside results
                     // 2. clear the error message and loading state (since our request just succeede)
@@ -33,7 +39,7 @@ var ajaxson5 = new Vue({
                 .catch(err => {
                     // if something went wrong, the code in here will execute instead of the success function
 
-                    this.loading = false;
+                   this.loading = false;
                     this.errorMessage = 'Sorry, could not load GIF. Try again!';
                 });
             // TODO We've just made a request, so this is a good time to
