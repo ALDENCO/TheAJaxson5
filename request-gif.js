55e2c252b7f1567455dc5d7859f1c6477405b4d3
ALDENCO <33554727+ALDENCO@users.noreply.github.com>
2018-08-24 14:36:08 -0600

    Add files via upload

--- request-gif.js
+++ request-gif.js
@@ -2,36 +2,39 @@ var ajaxson5 = new Vue({
     el: '#mount-point',
     data: function () {
         return {
-            tagValue: null,
+            tagValue: true,
             errorMessage: null,
-            loading: false,
-            imgSrc: null,
-            gif: gif.url,
+            loading: true,
+            imgSrc: null, 
+            //"https://78.media.tumblr.com/11b6c5922eefc306417b6993753c3e19/tumblr_o3f1npiGpD1r7eta3o1_500.gif",
+         ///   imgSrc: "https://api.giphy.com/v1/gifs/search?api_key=n2KJgt6XJN0Z9Fj6D8924vDw8vOAp5Tt&tag=${tag}",
+            
            
         };
     },
     methods: {
-        fetchGif: function() {
+        fetchGif: function(searchQuery,tag) {
             // get the user's input text from the DOM
-            var searchQuery = ""; // TODO should be e.g. "dance"
+            var searchQuery =""; // TODO should be e.g. "dance"
 
             // configure a few parameters to attach to our request
             var api_key = "n2KJgt6XJN0Z9Fj6D8924vDw8vOAp5Tt";
-            var tag = ""; // TODO should be e.g. "jackson 5 dance"
-            var gif_id = "gif.slug";
+            var tag = "michael jackson"; // TODO should be e.g. "jackson 5 dance"
+           
 
             // TODO what do we want this URL to be?
-            fetch(`https://api.giphy.com/v1/gifs/random?api_key=${api_key}&tag=${tag}`)
+            fetch(`https://api.giphy.com/v1/gifs/search?api_key=${api_key}&tag=${tag}&q=${searchQuery}`)
                 .then(response => response.ok ? response.json() : Promise.reject(response))
                 .then(results => {
                     // if the response comes back successfully, the code in here will execute.
 
-                    console.log("we received a response!");
-                    console.log(results);
+                    //console.log(tag);
+                    //console.log(searchQuery);
+                    // this.imgSrc=this.imgSrc(results.data[0]);
+                    this.imgSrc = results.data[0].images.original.url;
+                    
 
-                    this.imgSrc = results(gif_id);
-
-                    return `https://media.giphy.com/media/${gif_id}/giphy.gif`;
+                    
                     // TODO
                     // 1. set the imgSrc value in our data to the GIF's image_url inside results
                     // 2. clear the error message and loading state (since our request just succeede)
